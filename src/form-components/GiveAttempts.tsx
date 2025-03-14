3ae9531d4494969f9cd320c8d303697c3c1459a2
jfgordon <jfgordon@udel.edu>
2025-03-14 18:00:50 -0400

    giveAttempts works properly on site, failing some tests

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,54 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState(3);
+    const [attemptsWanted, setAttemptsWanted] = useState(0);
+
+    function handelAttempts(attemptsWanted: number) {
+        if (attemptsWanted < 0) {
+            return;
+        }
+        setAttempts(attemptsWanted);
+    }
     return (
         <div>
             <h3>Give Attempts</h3>
+
+            <p>Attempts left: {attempts}</p>
+            <Button
+                onClick={() => {
+                    if (attempts > 0) {
+                        setAttempts(attempts - 1);
+                    }
+                }}
+                disabled={attempts === 0}
+            >
+                use
+            </Button>
+            <Form.Group controlId="formAttemptsWanted">
+                <Form.Label>Enter Wanted Attempts:</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter answer"
+                    onChange={(e) => {
+                        if ((e.target as HTMLInputElement).value === "") {
+                            setAttemptsWanted(0);
+                        } else {
+                            setAttemptsWanted(
+                                parseInt((e.target as HTMLInputElement).value),
+                            );
+                        }
+                    }}
+                />
+            </Form.Group>
+            <Button
+                onClick={() => {
+                    handelAttempts(attemptsWanted);
+                }}
+            >
+                gain
+            </Button>
         </div>
     );
 }
