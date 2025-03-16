6e3df20903e5b37aa9420fcdace552309e1b1e72
jfgordon <jfgordon@udel.edu>
2025-03-16 02:52:03 -0400

    started ChangeColor

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>();
     return (
         <div>
+            <div></div>
             <h3>Change Color</h3>
         </div>
     );
