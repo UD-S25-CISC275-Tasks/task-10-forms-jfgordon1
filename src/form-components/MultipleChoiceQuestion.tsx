38440da41b3161a2ebd5d4dece4e3c665bce166f
jfgordon <jfgordon@udel.edu>
2025-03-16 21:55:54 -0400

    testing dropdown box

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [userChoice, setUserChoice] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setUserChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="MultipleChoice">
+                <Form.Label>Answer</Form.Label>
+                <Form.Select value={userChoice} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
         </div>
     );
 }
